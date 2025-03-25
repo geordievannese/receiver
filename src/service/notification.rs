c9dc9a5ee175092f33c03bda21eb8f89e3e8a82d
Geordie Vannese <geordievannese01@gmail.com>
2025-03-25 14:29:12 +0700

    Implement receive_notification function in Notification service

--- src/service/notification.rs
+++ src/service/notification.rs
@@ -90,4 +90,8 @@ use std::thread;
             ))
         }
     }
+    pub fn receive_notification(payload: Notification) -> Result<Notification> {
+        let subscriber_request: Notification = NotificationRepository::add(payload);
+        return Ok(subscriber_request);
+    }
  }
